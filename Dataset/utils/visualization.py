a82c0a879ac61f17eaf956c2eff2ac7f404af89a
junchi <wangjunchiseu@outlook.com>
2023-01-04 09:53:16 +0100

    update evalute script

--- Dataset/utils/visualization.py
+++ Dataset/utils/visualization.py
@@ -26,3 +26,19 @@ def draw_matching(image_1, image_2, matching, mask12):
                 full_image = cv2.line(full_image, src, dst, rand_color, 2)
 
     return full_image
+
+
+def draw_flow(image_1, flow):
+    image_flow = image_1.copy()
+    if image_1.dtype != np.uint8:
+        image_1 = (image_1 * 255.0).astype(np.uint8)
+
+    flow = flow.permute(1, 2, 0).cpu().numpy()
+    assert image_1.shape[:2] == flow.shape[:2]
+    flow_dire = flow[:, :, :2] / np.linalg.norm(flow[:, :, :2], axis=2, keepdims=True)
+    # drwa arrows on img1 according to flow12
+    for i in range(20, flow_dire.shape[0], 30):
+        for j in range(20, flow_dire.shape[1], 30):
+            cv2.arrowedLine(image_flow, (j, i), (int(j+flow_dire[i, j, 0]*15), int(i+flow_dire[i, j, 1]*15)), (255, 0, 0), 2, tipLength=0.4)
+
+    return image_flow
